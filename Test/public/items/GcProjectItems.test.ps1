8e2201c62edc8913125700319bad997057169e53
Raúl (Dibildos) González <rulasg@github.com>
2026-03-09 20:31:38 +0100

    feat(items): add Get-GcProjectItems function for project item retrieval

---
+++ Test/public/items/GcProjectItems.test.ps1
@@ -0,0 +1,59 @@
+function Test_GetProjectItems_Success{
+
+    MockCall_GetAllItems
+
+    # All 
+    # Act
+    $result = Get-GcProjectItems
+    # Assert
+    Assert-Count -Expected 5 -Presented $result
+
+    # IncludeDone
+    # Act
+    $result = Get-GcProjectItems -IncludeDone
+    # Assert
+    Assert-Count -Expected 6 -Presented $result
+
+    # RepositoryName
+    # Act
+    $result = Get-GcProjectItems -RepositoryName "kk"
+    # Assert
+    Assert-Count -Expected 2 -Presented $result
+
+    # ProjectNumber
+    # Act
+    $result = Get-GcProjectItems -ProjectNumber 2683
+    # Assert
+    Assert-Count -Expected 2 -Presented $result
+    Assert-Contains -Presented $result.id -Expected "PVTI_lADOAOnouM4AzN5Wzgml8mc"
+    Assert-Contains -Presented $result.id -Expected "PVTI_lADOAOnouM4AzN5WzgmP5nc"
+    
+    # Filter
+    # Act
+    $result = Get-GcProjectItems -Filter "Demo"
+    # Assert
+    Assert-Count -Expected 1 -Presented $result
+    Assert-Contains -Presented $result.id -Expected "PVTI_lADOAOnouM4AzN5WzgmP5nc"
+
+
+}
+
+function MockCall_GetProject($ProjectNumber){
+        MockCallJson -Command "Get-ProjectItems -owner githubcustomers -projectNumber $ProjectNumber -IncludeDone" -filename "get-projectitems-githubcustomers-$ProjectNumber.json"
+}
+
+function GetMockFiles($ProjectNumber){
+    enable-invokeCommandAliasModule
+
+    $cmd = "Get-ProjectItems -owner githubcustomers -projectNumber $ProjectNumber -IncludeDone -Force"
+    
+    save-invokeAsMockFile $cmd -FileName "get-projectitems-githubcustomers-$ProjectNumber.json"
+}
+
+function MockCall_GetAllItems{
+
+    MockCallToString -Command 'gh api user --jq ".login"' -OutString 'testuser'
+    MockCallJson -Command "Find-Project -owner githubcustomers -pattern creator:testuser" -filename "testuser-find-project-3.json"
+    MockCall_GetProject 2683 ; MockCall_GetProject 2988 ; MockCall_GetProject 3023
+
+}
---
+++ public/items/GcProjectItems.ps1
@@ -0,0 +1,57 @@
+function Get-GcProjectItems{
+    [CmdletBinding()]
+    [Alias ("scpi")]
+    param(
+        [Parameter(Position = 0)] [string[]]$Filter,
+        [Parameter(Position = 1)][string[]]$Attributes,
+        [Parameter()][string]$ProjectOwner,
+        [Parameter()][string]$ProjectNumber,
+        [Parameter()][switch]$IncludeDone,
+        [Parameter()][switch]$Force,
+        [Parameter()][switch]$PassThru,
+        # [Parameter()][string]$FieldName,
+        # [Parameter()][switch]$AnyField,
+        # [Parameter()][switch]$Exact
+        
+        [Parameter()][string]$RepositoryName
+        
+    )
+
+    $found = @((Get-AllItems -Force:$Force).Values)
+
+    # Owner and ProjectNumber filtering
+    if(-Not [string]::IsNullOrEmpty($Owner)){
+        $found = @($found | Where-Object {$_.projectOwner -eq $Owner})
+    }
+
+    # ProjectNumber filtering    if(-Not [string]::IsNullOrEmpty($ProjectNumber)){
+    if(-Not [string]::IsNullOrEmpty($ProjectNumber)){
+        $found = @($found | Where-Object {$_.projectNumber -eq $ProjectNumber})
+    }
+
+    #IncludeDone
+    if($IncludeDone){
+        $found = @($found)
+    } else {
+        $found = @($found | Where-Object {$_.Status -ne "Done"})
+    }
+
+    #RepositoryName
+    if(-Not [string]::IsNullOrEmpty($RepositoryName)){
+        $found = @($found | Where-Object {$_.RepositoryName -eq $RepositoryName})
+    }
+
+    #Filter
+    if(-Not [string]::IsNullOrEmpty($Filter)){
+        $found = @($found | Where-Object {$_.Title -match $Filter})
+    }
+
+
+    if($PassThru){
+        $ret = $found
+    } else {
+        $ret = $found | Format-ProjectItem -Attributes $Attributes
+    }
+
+    return $ret
+} Export-ModuleMember -Function Get-GcProjectItems -Alias scpi
