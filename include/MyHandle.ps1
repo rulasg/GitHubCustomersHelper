233c9e14d3314cbfba5d4872db5a09be73a98771
Raúl (Dibildos) González <rulasg@github.com>
2026-03-09 21:42:15 +0100

    fat(MyHandle): add cache to get-MyHangle

--- include/MyHandle.ps1
+++ include/MyHandle.ps1
@@ -1,10 +1,18 @@
 Set-MyInvokeCommandAlias -Alias GetGhHandle -Command 'gh api user --jq ".login"'
 
+$Script:myHandle = $null
+
 function Get-MyHandle{
     [CmdletBinding()]
     param()
+
+    if($null -ne $Script:myHandle){
+        return $Script:myHandle
+    }
     
     $user = Invoke-MyCommand -Command GetGhHandle
+
+    $Script:myHandle = $user
     
     return $user
 }
