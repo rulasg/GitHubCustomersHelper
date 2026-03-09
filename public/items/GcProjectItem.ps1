4647fbf770ac638eaf0cb6b608161678136a1bb2
Raúl (Dibildos) González <rulasg@github.com>
2026-03-09 22:48:18 +0100

    fix(Get-GcProjectItem) make it process

--- public/items/GcProjectItem.ps1
+++ public/items/GcProjectItem.ps1
@@ -5,18 +5,23 @@ function Get-GcProjectItem {
         [Parameter(Mandatory,ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)][Alias("id")][string]$ItemId
     )
 
-    $all = Get-AllItems
+    begin{
+        $all = Get-AllItems
+    }
 
-    if([string]::IsNullOrEmpty($ItemId)){
-        return $all
-    } else {
-        $item = $all.$ItemId
+    process{
 
-        if(-Not $item){
-            throw ("Item not found: "+$ItemId)
+        if([string]::IsNullOrEmpty($ItemId)){
+            return $all
+        } else {
+            $item = $all.$ItemId
+            
+            if(-Not $item){
+                throw ("Item not found: "+$ItemId)
+            }
+            
+            return $item
         }
-
-        return $item
     }
 
 } Export-ModuleMember -Function Get-GcProjectItem -Alias gcpi
