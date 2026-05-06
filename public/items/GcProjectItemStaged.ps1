b58670647cee42833e05beed3c366ba56cd82a2f
Raúl (Dibildos) González <rulasg@github.com>
2026-05-06 08:08:20 +0200

    feat(items): add TestProjectItemStaged command and integrate into Sync-GcProjectItemStaged function

--- public/items/GcProjectItemStaged.ps1
+++ public/items/GcProjectItemStaged.ps1
@@ -5,6 +5,8 @@ Set-MyInvokeCommandAlias -Alias ResetProjectItemStaged -Command 'Reset-ProjectIt
 Set-MyInvokeCommandAlias -Alias ResetProjectItemStagedWithItem -Command 'Reset-ProjectItemStaged -owner {owner} -ProjectNumber {projectnumber} -Id {itemid}'
 Set-MyInvokeCommandAlias -Alias SyncProjectItemStaged -Command 'Sync-ProjectItemStaged -owner {owner} -ProjectNumber {projectnumber}'
 
+Set-MyInvokeCommandAlias -Alias TestProjectItemStaged -Command 'Test-ProjectItemStaged -owner {owner} -ProjectNumber {projectnumber}'
+
 
 function Get-GcProjectItemStaged{
     [cmdletbinding()]
@@ -81,8 +83,11 @@ function Sync-GcProjectItemStaged{
         
         $params = @{owner=$project.Owner; projectnumber=$project.ProjectNumber}
 
-        Invoke-MyCommand -Command SyncProjectItemStaged -Parameters $params
+        $dirty = Invoke-MyCommand -Command TestProjectItemStaged -Parameters $params
 
+        if($dirty){
+            Invoke-MyCommand -Command SyncProjectItemStaged -Parameters $params
+        }
     }
 
 } Export-ModuleMember -Function Sync-GcProjectItemStaged -Alias ccommit
